9ce331f80360a67a9b0c3f38174270442c6402b6
alensci <ali.alkaraawi@crowdbotics.com>
2021-06-21 08:39:52 +0300

    3.13

--- Functions/make_fields_permissions.py
+++ Functions/make_fields_permissions.py
@@ -1,46 +1,47 @@
-
 from django.apps import AppConfig
-
-
-
-def make_fields_permissions(Permission,ContentType,Model):
+#
+#
+def make_fields_permissions(Permission, ContentType, Model):
     mode_name = Model.__name__.lower()
     permissions = []
     for item in Model._meta.fields:
-        permissions.append(('view_'+mode_name+'.'+item.name+'_field',
-                            'Can view '+item.name.replace('_', ' ')))
+        permissions.append(('view_' + mode_name + '.' + item.name + '_field',
+                            'Can view ' + item.name.replace('_', ' ')))
 
     for item in Model._meta.fields:
-        permissions.append(('change_'+mode_name+'.'+item.name+'_field',
-                            'Can change '+item.name.replace('_', ' ')))
+        permissions.append(('change_' + mode_name + '.' + item.name + '_field',
+                            'Can change ' + item.name.replace('_', ' ')))
     for codename, permission_name in permissions:
         Permission.objects.get_or_create(
-                codename=codename,
-                name=permission_name,
-                content_type=ContentType.objects.get_for_model(Model),
-            )
+            codename=codename,
+            name=permission_name,
+            content_type=ContentType.objects.get_for_model(Model),
+        )
+
+
 #
-
-
+#
+#
 from django.db import models
 
 
-# class YourAppConfig(AppConfig):
-#
-#     default_auto_field = 'django.db.models.AutoField'  # Don't modify, keep it as it is in your code
-#     name = 'Functions.make_fields_permissions'  # Don't modify, keep it as it is in your code
-#
-#     def ready(self):
-#         from django.contrib.auth.models import Permission
-#         from django.contrib.contenttypes.models import ContentType
-#         from django import apps
-#         # class Alert(models.Model):
-#         #     pass
-#         # Permission.objects.get_or_create(
-#         #     codename='view_alert',
-#         #     name="Can view alert",
-#         #     content_type=ContentType.objects.get_for_model(Alert)
-#         # )
-#         # for Model in apps.apps.get_models():
-#         #     make_fields_permissions(Permission, ContentType,Model)
+class YourAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'  # Don't modify, keep it as it is in your code
+    name = 'Functions.make_fields_permissions'  # Don't modify, keep it as it is in your code
 
+    def ready(self):
+        from django.contrib.auth.models import Permission
+        from django.contrib.contenttypes.models import ContentType
+        from django import apps
+        class Alert(models.Model):
+            pass
+        try:
+            Permission.objects.get_or_create(
+                codename='view_alert',
+                name="Can view alert",
+                content_type=ContentType.objects.get_for_model(Alert)
+            )
+            for Model in apps.apps.get_models():
+                make_fields_permissions(Permission, ContentType, Model)
+        except:
+            pass
--- test_28109/urls.py
+++ test_28109/urls.py
@@ -2,8 +2,27 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from rest_framework.documentation import include_docs_urls
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('', include_docs_urls(title='My API title')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('calendars/', include('calendars.urls')),
